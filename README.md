7edc15a43f20835e7586c1d294b81c85bf30de3f
Yikai Yang <yikai.yang@ymail.com>
2022-01-03 14:58:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# WLDataCleaner
+This repository provides a python script to merge the CSV Open Data resources [[1]](#1) provided by the Wiener Linien in preparation for easier access to the Realtime Monitor API [[2]](#2).
+
+## Why?
+To access the Realtime API by the Wiener Linien, it is necessary to transmit the station ids of the station to be viewed to the REST service. Each station can have multiple station ids (= platforms) in a 1:n relationship. However, the station ids are not listed in the file ``wienerlinien-ogd-haltestellen.csv`` [[3]](#3) (stations) directly, hence must be looked up in the file ``wienerlinien-ogd-haltepunkte.csv`` [[4]](#4) (platforms) for each station.
+
+The aim of this project is to combine the two aforementioned CSV ressources and provide a single csv or json as output with all the necessary information to access the Realtime API.
+
+## What does it do?
+The script matches the DIVA columns in the files ``wienerlinien-ogd-haltestellen.csv`` and ``wienerlinien-ogd-haltepunkte.csv``. Subsequently, it merges corresponding entries, similarly to a left outer join. Invalid values (entries containing null entries) are pruned priorly. Multiple geopositonal locations assigned to a single station are aggregated as an array.
+
+## Dependencies
+<ul>
+    <li>Python</li>
+    <li>pandas</li>
+    <li>plotly.express (Optional, if one wishes to view the output plotted on an interactive map) </li>
+</ul>
+
+## How to run
+Make sure a python environment is installed on your system.
+Download the linked csv files [[3]](#3), [[4]](#4) and place them in the same folder as the script.
+Install the dependencies specified in requirements.txt.
+
+``pip install requirements.txt``
+
+Run the script using the command:
+
+``python ./data_merger.py``
+
+## References
+<a href="https://www.data.gv.at/katalog/dataset/wiener-linien-echtzeitdaten-via-datendrehscheibe-wien" id="1">[1] https://www.data.gv.at/katalog/dataset/wiener-linien-echtzeitdaten-via-datendrehscheibe-wien</a> 
+
+<a href="https://www.data.gv.at/katalog/dataset/wiener-linien-echtzeitdaten-via-datendrehscheibe-wien/resource/ce15bc47-696f-4ff8-81f3-4ee0737d95de" id="2">[2] https://www.data.gv.at/katalog/dataset/wiener-linien-echtzeitdaten-via-datendrehscheibe-wien/resource/ce15bc47-696f-4ff8-81f3-4ee0737d95de</a> 
+
+<a href="http://www.wienerlinien.at/ogd_realtime/doku/ogd/wienerlinien-ogd-haltestellen.csv" id="3">[3] http://www.wienerlinien.at/ogd_realtime/doku/ogd/wienerlinien-ogd-haltestellen.csv</a> 
+
+<a href="http://www.wienerlinien.at/ogd_realtime/doku/ogd/wienerlinien-ogd-haltepunkte.csv" id="4">[4] http://www.wienerlinien.at/ogd_realtime/doku/ogd/wienerlinien-ogd-haltepunkte.csv</a> 
+
+
+## License
+Datenquelle: Stadt Wien - https://data.wien.gv.at
---
+++ data_merger.py
@@ -0,0 +1,75 @@
+#import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
+import re
+
+# Specifies whether surrounding quotation marks: " " are removed.
+removeQuotationMarksOption = True
+
+# Specifies whether S U appendixes after each station shall be purged.
+# Naming variants of stations will be combined. (e.g Stubentor U and Stubentor)
+removeAppendixesOption = True
+numberDecimalPlaces = 6
+
+removeQuotationMarks = lambda x: x.str.replace('"', '')
+removeAppendixes = lambda x: re.sub(removeAppendixesRegexStr, '', x)
+removeAppendixesRegexStr = ' S$| U$| S U$| S,| U,| S U,| SU$| SU,'
+removeInvalidCharRegex = lambda x: re.sub(removeInvalidCharRegexStr, '', x)
+removeInvalidCharRegexStr = '<|>'
+
+removeDummyStationsRegexStr = 'Dummy'
+def removeDummyStations(df):
+    filter = df['StopText'].str.contains(removeDummyStationsRegexStr)
+    print(filter)
+    df = df[~filter]
+    return df
+
+replaceDelimiterRegex = lambda x: re.sub(replaceDelimiterRegexStr, ', ', x)
+replaceDelimiterRegexStr = ' / | - '
+
+dfHP = pd.read_csv("./wienerlinien-ogd-haltepunkte.csv", delimiter=';', skiprows=0)
+dfHS = pd.read_csv('./wienerlinien-ogd-haltestellen.csv', delimiter=';', skiprows=0)
+
+dfHP= dfHP.dropna(axis=0, how="any")
+dfHP.drop(['StopText','Municipality', 'MunicipalityID'], axis=1, inplace=True)
+dfHS = dfHS.dropna(axis=0, how="any")
+dfHP = removeDummyStations(dfHP)
+
+dfResult = pd.merge(dfHP, dfHS, suffixes=['_hp', '_hs'], on= 'DIVA', how='inner')
+dfResult['Platforms'] = dfResult.apply(lambda x: [x['Longitude_hp'] ,x['Latitude_hp']], axis=1)
+
+dfResult = dfResult.apply({
+    'StopID': str,
+    'PlatformText': lambda x: replaceDelimiterRegex(removeInvalidCharRegex(removeAppendixes(x))),
+    'DIVA': str,
+    'Municipality': str,
+    'MunicipalityID': str,
+    'Latitude_hp': lambda x: x,
+    'Longitude_hp': lambda x: x,
+    'Latitude_hs': lambda x: x,
+    'Longitude_hs': lambda x: x,
+    'Platforms': lambda x: x,
+}).groupby(['DIVA']).agg({
+    'PlatformText': lambda x: x.iloc[0],
+    'StopID': ','.join,
+    'Municipality': lambda x: x.iloc[0],
+    'MunicipalityID': lambda x: x.iloc[0],
+    'Latitude_hs': lambda x: x.iloc[0],
+    'Longitude_hs': lambda x: x.iloc[0],
+    'Platforms': lambda x: list(x)
+})
+
+dfResult = dfResult.rename(columns={'Latitude_hs': 'Latitude', 'Longitude_hs': 'Longitude'})
+dfResult.to_csv('./wienerlinien-ogd-haltepunkte_merged.csv', index = False, sep=';')
+
+
+# Uncomment to plot station locations on interactive map
+
+# def showMap():
+#     fig = px.scatter_mapbox(dfResult, lat="Latitude", lon="Longitude", hover_name="PlatformText", hover_data=["Longitude", "Latitude"])
+#     fig.update_layout(mapbox_style="open-street-map")
+#     fig.show()
+#showMap() 
+
+dfResult.to_csv('./wienerlinien-ogd-haltepunkte_merged.csv', index = False, sep=';')
+dfResult.to_json('./wienerlinien-ogd-haltepunkte_merged.json', orient = 'records')
